214ae7ccdcc74c158fd0d8276b7bbe9d6fe7425f
JaretWright <hkkA223>
2018-10-19 13:06:07 -0400

    Added ability to send a Phone object into the PhoneViewController

--- src/Controllers/TableOfPhonesViewController.java
+++ src/Controllers/TableOfPhonesViewController.java
@@ -4,16 +4,23 @@ import Models.DBConnect;
 import Models.Phone;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Node;
 
 public class TableOfPhonesViewController implements Initializable {
     @FXML
@@ -29,7 +36,7 @@ public class TableOfPhonesViewController implements Initializable {
     private TableColumn<Phone, String> osColumn;
 
     @FXML
-    private TableColumn<Phone, Integer> memoryColumn;
+    private TableColumn<Phone, Double> memoryColumn;
 
 
     @Override
@@ -38,17 +45,37 @@ public class TableOfPhonesViewController implements Initializable {
         makeColumn.setCellValueFactory(new PropertyValueFactory<Phone, String>("make"));
         modelColumn.setCellValueFactory(new PropertyValueFactory<Phone, String>("model"));
         osColumn.setCellValueFactory(new PropertyValueFactory<Phone, String>("os"));
-        memoryColumn.setCellValueFactory(new PropertyValueFactory<Phone, Integer>("memory"));
+        memoryColumn.setCellValueFactory(new PropertyValueFactory<Phone, Double>("memory"));
 
-//        try {
-//            tableView.getItems().addAll(DBConnect.getAllPhones());
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            tableView.getItems().addAll(DBConnect.getAllPhones());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     public void viewSelectedPhone(ActionEvent event) throws IOException {
+        Phone phoneSelected = tableView.getSelectionModel().getSelectedItem();
 
+        //check to ensure that a phone was selected before changing scenes
+        if (phoneSelected != null)
+        {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("Views/PhoneView.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+
+            //access the controller class and load the Phone object
+            PhoneViewController controller = loader.getController();
+            controller.loadPhone(phoneSelected);
+
+            //get the Stage and set the scene/show
+            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            stage.setTitle("Phone View");
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 }
